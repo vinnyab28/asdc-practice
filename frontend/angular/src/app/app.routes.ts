08b6644a257fbdaad1528cd924c5c7fa114718bd
Vineeth Prakash <gudipallivineethprakash@karunya.edu.in>
2024-05-14 23:04:33 -0300

    Added the routes

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { UsersComponent } from './components/users/users.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomepageComponent
+    },
+    {
+        path: "users",
+        component: UsersComponent
+    },
+    {
+        path: "products",
+        component: ProductsComponent
+    },
+    {
+        path: "products/:id",
+        component: ProductDetailsComponent
+    },
+    {
+        path: "**",
+        component: HomepageComponent
+    },
+];
