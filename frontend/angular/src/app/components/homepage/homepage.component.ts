975f80caf312f745ce99d3d187684dc87fdabf9d
Vineeth Prakash <gudipallivineethprakash@karunya.edu.in>
2024-05-14 23:20:39 -0300

    Updated Homepage component

--- frontend/angular/src/app/components/homepage/homepage.component.ts
+++ frontend/angular/src/app/components/homepage/homepage.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, RouterLink],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
+  constructor(private router: Router) { }
 
+  onShowUsers() {
+    this.router.navigate(["/", "users"]);
+  }
 }
